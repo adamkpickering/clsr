a4762bca4bc5e6f3bd493aa37e7c94c40299f565
Adam Pickering <adamkpickering@gmail.com>
2021-10-18 17:02:56 -0600

    Remove some dead code

--- models/deck.go
+++ models/deck.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"fmt"
-	"os"
 	"path/filepath"
 )
 
@@ -11,36 +9,6 @@ import (
 type Deck struct {
 	Name  string
 	Cards []*Card
-}
-
-func LoadDeck(path string) (*Deck, error) {
-	// get a list of card paths
-	entries, err := os.ReadDir(path)
-	if err != nil {
-		return &Deck{}, fmt.Errorf("failed to read directory %s: %s", path, err)
-	}
-
-	// load each card in the deck
-	deck := &Deck{
-		Name: filepath.Base(path),
-	}
-	for _, entry := range entries {
-		entryInfo, err := entry.Info()
-		if err != nil {
-			return &Deck{}, fmt.Errorf("failed to get info for entry %s: %s", entry.Name(), err)
-		}
-		if entryInfo.IsDir() {
-			continue
-		}
-		cardPath := filepath.Join(path, entry.Name())
-		card, err := parseCardFromFile(cardPath)
-		if err != nil {
-			return &Deck{}, fmt.Errorf("failed to parse card %s: %s", cardPath, err)
-		}
-		deck.Cards = append(deck.Cards, card)
-	}
-
-	return deck, nil
 }
 
 func NewDeck(path string) (*Deck, error) {
