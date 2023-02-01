fa15a5c9c6308b595bc9cd6d8bf5458080041ea6
Adam Pickering <adam.pickering@suse.com>
2023-02-01 09:40:35 -0700

    Make `clsr set card` a subcommand of `clsr set`

--- cmd/set.go
+++ cmd/set.go
@@ -22,91 +22,12 @@ THE SOFTWARE.
 package cmd
 
 import (
-	"errors"
-	"fmt"
-	"os"
-
-	"github.com/adamkpickering/clsr/pkg/deck_source"
-	"github.com/adamkpickering/clsr/pkg/models"
 	"github.com/spf13/cobra"
 )
 
 var setCmd = &cobra.Command{
-	Use:   "set <resource_type> <resource_id> <state>",
-	Short: "Set the state of various resource types",
-	Long: `
-Sets the state of various resource types.
-
-There are two states for cards: "active", and "inactive".
-"active" means that it will show up when studying cards.
-"inactive" means that it will not.`,
-	Args: cobra.ExactArgs(3),
-	RunE: func(cmd *cobra.Command, args []string) error {
-		deckName := *cmd.PersistentFlags().StringP("deck", "d", "", "the deck to which the card will belong")
-
-		// check that the directory has been initialized
-		if _, err := os.Stat(deckDirectory); errors.Is(err, os.ErrNotExist) {
-			return fmt.Errorf("Could not find %s. Please call `clsr init` and try again.", deckDirectory)
-		}
-
-		// construct DeckSource
-		deckSource, err := deck_source.NewJSONFileDeckSource(deckDirectory)
-		if err != nil {
-			return fmt.Errorf("failed to construct DeckSource: %w", err)
-		}
-
-		// get deck
-		if len(deckName) == 0 {
-			return errors.New("must specify --deck/-d")
-		}
-		deck, err := deckSource.ReadDeck(deckName)
-		if err != nil {
-			return fmt.Errorf("failed to read deck %q: %w", deckName, err)
-		}
-
-		// get the card from the deck
-		cardID := args[1]
-		card := &models.Card{}
-		for _, deckCard := range deck.Cards {
-			if deckCard.ID == cardID {
-				card = deckCard
-				break
-			}
-		}
-		if len(card.ID) == 0 {
-			return fmt.Errorf("could not find card with ID %q", cardID)
-		}
-
-		resourceType := args[0]
-		adjective := args[2]
-		switch resourceType {
-		case "card":
-			switch adjective {
-			case "active":
-				if !card.Active {
-					card.Active = true
-					card.Modified = true
-				}
-			case "inactive":
-				if card.Active {
-					card.Active = false
-					card.Modified = true
-				}
-			default:
-				return fmt.Errorf("invalid adjective %q", adjective)
-			}
-		default:
-			return fmt.Errorf("invalid resource type %q", resourceType)
-		}
-
-		// write deck
-		err = deckSource.WriteDeck(deck)
-		if err != nil {
-			return fmt.Errorf("failed to write deck %q: %w", deck.Name, err)
-		}
-
-		return nil
-	},
+	Use:   "set",
+	Short: "Set attributes of resources",
 }
 
 func init() {
