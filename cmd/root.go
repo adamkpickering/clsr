debc303a8985a71ab959b1bf0cc36115df9525be
Adam Pickering <adam.pickering@suse.com>
2023-03-04 06:55:54 -0700

    Add --version flag

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,7 @@
 /*
 Copyright © 2021 ADAM PICKERING
 
- ermission is hereby granted, free of charge, to any person obtaining a copy
+Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
@@ -39,16 +39,19 @@ It schedules cards according to the principle of spaced repetition
 so that you learn most efficiently.`,
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
-}
-
 func init() {
 	localDeckDirectory, err := os.Getwd()
 	if err != nil {
 		panic(fmt.Errorf("failed to get working directory: %w", err))
 	}
 	deckDirectory = localDeckDirectory
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func SetVersionInfo(version string) {
+	rootCmd.Version = version
+	rootCmd.SetVersionTemplate("{{ .Version }}\n")
 }
--- main.go
+++ main.go
@@ -23,6 +23,9 @@ package main
 
 import "github.com/adamkpickering/clsr/cmd"
 
+var version = "development"
+
 func main() {
+	cmd.SetVersionInfo(version)
 	cmd.Execute()
 }
